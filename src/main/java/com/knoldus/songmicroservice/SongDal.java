d09095cb5adf64b3af49d14fc3fbb739be190d49
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-02-06 00:35:08 +0530

    initial commit

---
+++ src/main/java/com/knoldus/songmicroservice/SongDal.java
@@ -0,0 +1,9 @@
+package com.knoldus.songmicroservice;
+
+public interface SongDal {
+	DbQueryStatus addSong(Song songToAdd);
+	DbQueryStatus findSongById(String songId);
+	DbQueryStatus getSongTitleById(String songId);
+	DbQueryStatus deleteSongById(String songId);	
+	DbQueryStatus updateSongFavouritesCount(String songId, boolean shouldDecrement);
+}
